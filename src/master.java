e68c9762f2f564eb9c63e28bc4c7a35ce699e475
Argo12 <argo12@ptc.local>
2019-12-23 14:02:26 +0200

    b4 cm 1

--- src/master.java
+++ src/master.java
@@ -6,5 +6,6 @@ public static void main(String[] args){
 		System.out.println(" cm 2");
 		System.out.println(" cm 3");
 		System.out.println(" cm 4 + readme ");
+		System.out.println("  b4  cm 1");
 	}
 }
