abcb47882a3441741ecdef9dd912e8ce6f1be53f
Argo12 <argo12@ptc.local>
2019-12-23 14:04:31 +0200

    master commit 2

--- src/master.java
+++ src/master.java
@@ -7,6 +7,7 @@ public static void main(String[] args){
 		System.out.println(" cm 3");
 		System.out.println(" cm 4 + readme ");
 		System.out.println(" master cm 1");
+		System.out.println(" master cm 2");
 		System.out.println(" cm 5");
 		System.out.println(" cm 6");
 		System.out.println(" cm 7");
