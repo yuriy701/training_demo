f6442aa882de4c7d3cf6c589304782d36e690eaa
Argo12 <argo12@ptc.local>
2019-12-23 12:00:58 +0200

    commit 4 and add readme

--- src/master.java
+++ src/master.java
@@ -5,5 +5,6 @@ public static void main(String[] args){
 		System.out.println(" cm 1");
 		System.out.println(" cm 2");
 		System.out.println(" cm 3");
+		System.out.println(" cm 4 + readme ");
 	}
 }
