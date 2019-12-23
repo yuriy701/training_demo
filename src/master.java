d3fc80d72c4579ad0a5a17054bbd238943f91753
Argo12 <argo12@ptc.local>
2019-12-23 14:03:18 +0200

    b4 cm 2

--- src/master.java
+++ src/master.java
@@ -7,5 +7,6 @@ public static void main(String[] args){
 		System.out.println(" cm 3");
 		System.out.println(" cm 4 + readme ");
 		System.out.println("  b4  cm 1");
+		System.out.println("  b4  cm 2");
 	}
 }
