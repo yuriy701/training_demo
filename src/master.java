700c470e26f7961700620ac0e8734bf81fce0103
Argo12 <argo12@ptc.local>
2019-12-24 11:39:56 +0200

    cm 15

--- src/master.java
+++ src/master.java
@@ -25,6 +25,7 @@ public static void main(String[] args){
 		System.out.println(" cm 12");
 		System.out.println(" cm 13");
 		System.out.println(" cm 14");
+		System.out.println(" cm 15");
 		
 	}
 }
