6eaaa52e259fcee944c21c216022b245ba25913a
Argo12 <argo12@ptc.local>
2019-12-24 14:31:02 +0200

    reb test 3

--- src/master.java
+++ src/master.java
@@ -29,8 +29,10 @@ public static void main(String[] args){
 		System.out.println(" 20 ");
 		
 		System.out.println(" commit 300 ");
-		
 
+		System.out.println(" reb test 1 ");
+		System.out.println(" reb test 2 ");
+		System.out.println(" reb test 3 ");
 
 	}
 }
