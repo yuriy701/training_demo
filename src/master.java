3d8ddc344970e8473e8e392e1615cfc96e50bb4b
yuriy701 <kyi@bigmir.net>
2019-12-24 11:49:19 +0200

    GH lb 15

--- src/master.java
+++ src/master.java
@@ -25,6 +25,7 @@ public static void main(String[] args){
 		System.out.println(" cm 12");
 		System.out.println(" lb 13");
 		System.out.println(" lb 14");
+		System.out.println(" GHlb 15");
 		
 	}
 }
